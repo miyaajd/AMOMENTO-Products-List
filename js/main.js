91b76c062133e4782b6ff845cde037d4d6b66dda
miyaajd <ehdal1032@gmail.com>
2025-08-25 15:54:33 +0900

    a tag edit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 window.addEventListener("load", function () {
+  const aTag = this.document.querySelectorAll("a");
+  aTag.forEach((a) => {
+    a.addEventListener("click", (e) => {
+      if (a.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
   const copyBtn = this.document.querySelector("#copyMore");
   const copyTxt = this.document.querySelector(".copyTxt");
   copyBtn.addEventListener("click", (e) => {
